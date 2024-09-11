5362144090675590eeb33240bb39cc751208330d
epolon <epolon@amazon.com>
2024-09-11 16:57:43 +0300

    mid work

--- .projenrc.ts
+++ .projenrc.ts
@@ -23,8 +23,8 @@ const project = new CdklabsTypeScriptProject({
     '',
     '### Ask Yourself',
     '',
-    '[ ] Have you reviewed the contribution guide?',
-    '[ ] Have you reviewed the breaking changes guide?',
+    '[ ] Have you reviewed the [contribution guide](https://github.com/cdklabs/json2jsii/blob/main/CONTRIBUTING.md)?',
+    '[ ] Have you reviewed the [breaking changes guide](https://github.com/cdklabs/json2jsii/blob/main/CONTRIBUTING.md#breaking-changes)?',
     '',
   ],
 });
