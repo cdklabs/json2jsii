e2669b56ada3b1a7cdfc5408c45c0c9e80818f1f
Momo Kornher <kornherm@amazon.co.uk>
2025-06-20 15:59:31 +0100

    feat: (opt-in) unions of a type and an array of that same type can be the array type

--- src/transformers/index.ts
+++ src/transformers/index.ts
@@ -2,3 +2,4 @@ export * from './null-in-type-array';
 export * from './null-in-union';
 export * from './union-duplicates';
 export * from './singleton-unions';
+export * from './array-union-reducer';
